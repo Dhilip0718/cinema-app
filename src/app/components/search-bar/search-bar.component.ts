de70a3ed077918fe07207fcf8bf74cbd4af44c46
Dhilip <dhilip@Dhiviyas-Air.home>
2025-02-12 16:43:06 +0100

    updated the search and clear behaviour

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { Movie, MovieService } from '../../services/movie.service';
 import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from "../movie-card/movie-card.component";
+import { FeaturedComponent } from '../featured/featured.component';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [FormsModule, CommonModule, MovieCardComponent,],
+  imports: [FormsModule, CommonModule, MovieCardComponent,FeaturedComponent],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
   standalone: true,
@@ -18,7 +19,8 @@ import { MovieCardComponent } from "../movie-card/movie-card.component";
 export class SearchBarComponent {
   searchTerm: string = '';
   movies: Movie[] = [];
-  plotLength: string = 'full'; // Default plot length
+  plotLength: string = 'full';
+  showFeatured = true;
 
   constructor(private movieService: MovieService) {}
 
@@ -39,6 +41,12 @@ export class SearchBarComponent {
     } else {
       this.movies = []; // Clear results if search term is empty
     }
+  }
+  clearSearch() {
+    this.searchTerm = '';
+    this.movies = [];
+    this.showFeatured = true;
+
   }
 
   togglePlotLength() {
